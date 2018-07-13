51a2bb544ae0eb4b8c9e49f046db1f92bbd63ccf
Chloe Collier <achloecollier@gmail.com>
2018-07-13 16:42:17 -0700

    Toggling template views

--- js/page-view.js
+++ js/page-view.js
@@ -11,17 +11,48 @@ const pageViewPrototype = {
         const templateContainer = document.createElement('div');
         templateContainer.className = 'c-main';
         const data = {title: "My New Post", body: "This is my first post!"};
-        templateContainer.innerHTML = homeTemplate(data);
+        templateContainer.innerHTML = this._template(data);
         const mainElement = document.querySelector('main');
+
+        this._removePreviousView(); 
         mainElement.append(templateContainer);
+    },
+
+    _onClickChangeTemplate: function(e) {
+        this._template = this._getTemplate(e);
+        this.render();
+    },
+
+    _getTemplate: function(e) {
+        const clickedTemplate = e.target.dataset.link;
+
+        if (clickedTemplate === 'home') {
+            return homeTemplate;
+        }
+        if (clickedTemplate === 'about') {
+            return aboutTemplate;
+        }
+        if (clickedTemplate === 'contact') {
+            return contactTemplate;
+        }
+    },
+
+    _removePreviousView: function() {
+        const previousView = this._mainElement.querySelector('.c-main');
+        if (previousView) {
+            previousView.remove();
+        }
     }
 }
 
-export const create = function (mainElement, state) {
+export const create = function (mainElement, footerElement, state) {
     const view = Object.create(pageViewPrototype);
     view._mainElement = mainElement;
-    view._template = aboutTemplate;
+    view._template = homeTemplate;
     view.state = state;
+
+    const links = footerElement.querySelectorAll('.js-link');
+    links.forEach(link => link.addEventListener('click', view._onClickChangeTemplate.bind(view)));
 
     return view;
 }
--- js/page.js
+++ js/page.js
@@ -4,8 +4,9 @@ import * as PageView from './page-view';
 import * as PageState from './page-state';
 
 document.addEventListener('DOMContentLoaded', function () {
-    const mainElement = document.querySelector('.c-main');
+    const mainElement = document.querySelector('.c-content');
+    const footerElement = document.querySelector('.c-footer');
     const view = PageView;
     const state = PageState.create();
-    view.create(mainElement, state).render();
+    view.create(mainElement, footerElement, state).render();
 });
